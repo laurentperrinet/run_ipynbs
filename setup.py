9097b784534e68e0e507acbb739b2e4c38df30e4
HadiM <marelo64@gmail.com>
2013-12-12 13:44:38 +0100

    add setup.py

--- setup.py
+++ setup.py
@@ -1,36 +1,28 @@
 from setuptools import setup
 
 setup(name='run_ipynbs',
-      version='0.8.0-dev',
-      license='MIT',
-      description='Subtitles, faster than your thoughts',
-      long_description=open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
-      keywords='subtitle subtitles video movie episode tv show',
-      url='https://github.com/Diaoul/subliminal',
-      author='Antoine Bertin',
-      author_email='diaoulael@gmail.com',
-      packages=find_packages(),
-      classifiers=['Development Status :: 4 - Beta',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: MIT License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
+      version='1.0',
+      license='GPLv3',
+      description='Run non-interactively ipython notebook files',
+      long_description=open('README.md').read(),
+      keywords='ipython, notebooks, non-interactive, console',
+      url='https://github.com/hadim/run_ipynbs',
+      author='HadiM',
+      author_email='hadrien.mary@gmail.com',
+      classifiers=[
+          'Framework :: IPython',
           'Programming Language :: Python :: 2',
           'Programming Language :: Python :: 2.7',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.3',
-          'Topic :: Software Development :: Libraries :: Python Modules',
-          'Topic :: Multimedia :: Video'],
-      entry_points={
-          'console_scripts': ['subliminal = subliminal.cli:subliminal'],
-          'subliminal.providers': ['addic7ed = subliminal.providers.addic7ed:Addic7edProvider',
-                                   'opensubtitles = subliminal.providers.opensubtitles:OpenSubtitlesProvider',
-                                   'podnapisi = subliminal.providers.podnapisi:PodnapisiProvider',
-                                   'thesubdb = subliminal.providers.thesubdb:TheSubDBProvider',
-                                   'tvsubtitles = subliminal.providers.tvsubtitles:TVsubtitlesProvider'],
-          'babelfish.language_converters': ['addic7ed = subliminal.converters.addic7ed:Addic7edConverter',
-                                            'podnapisi = subliminal.converters.podnapisi:PodnapisiConverter',
-                                            'tvsubtitles = subliminal.converters.tvsubtitles:TVsubtitlesConverter']
-      },
-      install_requires=open('requirements.txt').readlines(),
-      test_suite='subliminal.tests.suite')
+      ],
+      install_requires=[
+          'Jinja2',
+          'Pygments',
+          'ipython',
+          'pyzmq',
+      ],
+      scripts=[
+          'run_ipynbs.py'
+      ]
+      )
